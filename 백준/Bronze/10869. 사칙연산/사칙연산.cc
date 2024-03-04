54ebbd2c83c82cfdb7cd2e12b999b51e23e8897f
leastzero <140035819+leastzero@users.noreply.github.com>
2024-03-04 23:34:36 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+
+	A >= 1; B <= 10000;
+
+	cin >> A >> B;
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B;
+
+	return 0;
+}
