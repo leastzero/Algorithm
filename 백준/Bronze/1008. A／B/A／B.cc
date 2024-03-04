4c99c38c251df50fd3af4704605862f72311e884
leastzero <140035819+leastzero@users.noreply.github.com>
2024-03-04 23:30:48 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double A, B;
+    A > 0; B < 10;
+    
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / B;
+    
+    return 0;
+}
