7b55c6236405b272c08f25b418104b9426d7d1ad
leastzero <140035819+leastzero@users.noreply.github.com>
2024-03-20 00:01:37 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	int** A = new int* [N];
+	int** B = new int* [N];
+	for (int i = 0; i < N; i++) {
+		A[i] = new int[M];
+		B[i] = new int[M];
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> A[i][j];
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> B[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << A[i][j] + B[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < N; i++) {
+		delete[] A[i];
+		delete[] B[i];
+	}
+
+	delete[] A;
+	delete[] B;
+
+	return 0;
+}
