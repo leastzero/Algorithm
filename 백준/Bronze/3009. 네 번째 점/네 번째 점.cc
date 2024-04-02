4ce4aab2f93516a82e4d5ac45fa675e4bd0614a2
leastzero <140035819+leastzero@users.noreply.github.com>
2024-04-03 00:24:50 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x[3];
+	int y[3];
+
+	for (int i = 0; i < 3; i++) {
+		cin >> x[i] >> y[i];
+	}
+
+	if (x[0] == x[1]) cout << x[2] << " ";
+	else if (x[1] == x[2]) cout << x[0] << " ";
+	else cout << x[1] << " ";
+
+	if (y[0] == y[1]) cout << y[2];
+	else if (y[1] == y[2]) cout << y[0];
+	else cout << y[1];
+
+	return 0;
+}
