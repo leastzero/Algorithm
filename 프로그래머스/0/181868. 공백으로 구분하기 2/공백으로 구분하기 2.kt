52d101a799067b2c9b24ec7716212aaed11c4ebf
leastzero <140035819+leastzero@users.noreply.github.com>
2024-05-02 00:37:16 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 13.78 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        answer = my_string.split(" ").filter {
+            it.isNotEmpty()
+        }.toTypedArray()
+        return answer
+    }
+}
