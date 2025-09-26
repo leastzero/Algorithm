e46856df277d095b027ee9d2863d9682fb6e65b6
leastzero <140035819+leastzero@users.noreply.github.com>
2025-09-26 22:40:32 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+
+    for i in range(len(d)):
+        budget = budget - d[i]
+        if budget < 0:
+            break
+        answer += 1
+    return answer
