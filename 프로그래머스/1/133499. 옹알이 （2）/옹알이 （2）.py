25c583411ef25cbb98324b6d370a656e48e99941
leastzero <140035819+leastzero@users.noreply.github.com>
2025-10-18 18:01:45 +0900

    [level 1] Title: 옹알이 (2), Time: 0.13 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,13 @@
+def solution(babbling):
+    answer = 0
+    words = ["aya", "ye", "woo", "ma"]
+    
+    for bab in babbling:
+        for word in words:
+            if word * 2 not in bab:
+                bab = bab.replace(word, " ")
+                
+        if bab.isspace():
+            answer += 1
+            
+    return answer
