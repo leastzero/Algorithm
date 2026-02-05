147554adcc26cd2230d182b96fd8bd9df168ea71
최소영 <csong7177@naver.com>
2026-02-05 17:32:36 +0900

    [Silver III] Title: 타노스, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,19 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-s = input().strip()
+s = input()
 
-dict = {'0': 0, '1': 0}
+zero = s.count('0') // 2
+one = s.count('1') // 2
 
-for x in s:
-    dict[x] += 1
+s_list = list(s)
+cnt_zero = 0
+cnt_one = 0
 
-for key, value in dict.items():
-    dict[key] = value // 2
+# 0 제거
+for i in range(len(s_list)-1, -1, -1): # 뒤에서부터 0 제거 (사전순이니까)
+    if s_list[i] == '0':
+        s_list[i] = ''
+        cnt_zero += 1
 
-answer = ''
-for key, value in dict.items():
-    for _ in range(value):
-        answer += key
+        if cnt_zero == zero:
+            break
 
-print(answer)
+# 1 제거
+for i in range(len(s_list)): # 앞에서부터 1 제거 (사전순이니까)
+    if s_list[i] == '1':
+        s_list[i] = ''
+        cnt_one += 1
+
+        if cnt_one == one:
+            break
+
+print(''.join(s_list))
