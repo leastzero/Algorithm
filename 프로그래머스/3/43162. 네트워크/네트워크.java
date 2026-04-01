91a1db7ae74f411903ed8e44189697ac5fba60e4
최소영 <csong7177@naver.com>
2026-04-01 16:42:37 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,30 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
+                dfs(i, n, computers, visited);
                 answer++;
-                
-                q.offer(i);
-                visited[i] = true;
-                
-                while (!q.isEmpty()) {
-                    int cur = q.poll();
-                    
-                    for (int j = 0; j < n; j++) {
-                        if (computers[cur][j] == 1 && !visited[j]) {
-                            visited[j] = true;
-                            q.offer(j);
-                        }
-                    }
-                }
             }
         }
         
         return answer;
+    }
+    
+    private void dfs(int i, int n, int[][] computers, boolean[] visited) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            
+            if (computers[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(j, n, computers, visited);
+            }
+        }
     }
 }
