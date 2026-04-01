2ef241ebe71036e7cdb334f8b10fdbf0c2ad4499
최소영 <csong7177@naver.com>
2026-04-01 22:26:18 +0900

    [level 3] Title: 여행경로, Time: 0.81 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,41 +1,40 @@
 import java.util.*;
 
 class Solution {
-    String[] answer;
-    boolean[] visited;
+    String[] answer = {};
+    List<String> path = new ArrayList<>();
     boolean found = false;
     
     public String[] solution(String[][] tickets) {
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1])); // 알파벳 순서로 정렬
         
-        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        boolean[] visited = new boolean[tickets.length]; // 티켓 사용 여부
         
-        visited = new boolean[tickets.length];
-        List<String> path = new ArrayList<>();
         path.add("ICN");
-        
-        dfs("ICN", tickets, path, 0);
+        dfs("ICN", tickets, visited, 0);
         
         return answer;
     }
     
-    private void dfs(String now, String[][] tickets, List<String> path, int count) {
-        if (found) return;
+    private void dfs(String cur, String[][] tickets, boolean[] visited, int depth) {
+        if (found) return; // 알파벳 정렬을 했으므로 최초로 티켓을 다 사용한 경우가 정답
         
-        if (count == tickets.length) {
-            answer = path.toArray(new String[0]);
+        if (depth == tickets.length) {
+            answer = path.toArray(new String[0]); // 리스트를 배열로 변환
             found = true;
             return;
         }
         
         for (int i = 0; i < tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(now)) {
+            if (!visited[i] && tickets[i][0].equals(cur)) {
                 visited[i] = true;
                 path.add(tickets[i][1]);
                 
-                dfs(tickets[i][1], tickets, path, count + 1);
+                dfs(tickets[i][1], tickets, visited, depth + 1);
                 
-                path.remove(path.size() - 1);
+                // 모든 티켓을 다 사용하기 전에 경로가 끊기는 경우 백트래킹
                 visited[i] = false;
+                path.remove(path.size() - 1);
             }
         }
     }
