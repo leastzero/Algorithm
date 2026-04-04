c2a850dd1cac1c67b071898b08232e3424586aab
최소영 <csong7177@naver.com>
2026-04-04 18:03:04 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.75 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,39 +4,39 @@ class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        ArrayList<Integer>[] board = new ArrayList[n+1];
+        List<Integer>[] graph = new ArrayList[n+1];
         for (int i = 1; i < n+1; i++) {
-            board[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] e: edge) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
         }
         
         int[] dist = new int[n+1];
         Arrays.fill(dist, -1);
         
-        for (int[] e: edge) {
-            board[e[0]].add(e[1]);
-            board[e[1]].add(e[0]);
-        }
-        
-        Queue<Integer> q = new ArrayDeque();
+        Queue<Integer> q = new ArrayDeque<>();
         q.offer(1);
         dist[1] = 0;
         int distance = 0;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int cur = q.poll();
             
-            for (int next: board[cur]) {
+            for (int next: graph[cur]) {
                 if (dist[next] == -1) {
                     dist[next] = dist[cur] + 1;
-                    distance = Math.max(distance, dist[next]);
                     q.offer(next);
+                    distance = Math.max(distance, dist[next]);
                 }
             }
         }
         
-        for (int i = 0; i < n+1; i++) {
-            if (distance == dist[i]) {
-                answer += 1;
+        for (int d: dist) {
+            if (d == distance) {
+                answer++;
             }
         }
         
