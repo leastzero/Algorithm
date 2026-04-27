46dd8692f18c70b9ed03b7bdabe5ecd416d89767
최소영 <csong7177@naver.com>
2026-04-27 16:31:33 +0900

    [level 2] Title: 숫자의 표현, Time: 1.46 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                
+                if (sum > n) break;
+                
+                if (sum == n) {
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    
+}
